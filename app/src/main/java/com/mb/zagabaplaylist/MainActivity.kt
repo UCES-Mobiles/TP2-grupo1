6ed9474fb495a7b383262555eb8a11e8de90ae08
damian.zamora <damian.zamora@hotmail.com>
2025-06-21 11:56:53 -0300

    se agrega logo en el main principal

--- app/src/main/java/com/mb/zagabaplaylist/MainActivity.kt
+++ app/src/main/java/com/mb/zagabaplaylist/MainActivity.kt
@@ -2,6 +2,8 @@ package com.mb.zagabaplaylist
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,10 +20,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
-        startActivity(Intent(this, AddSongActivity::class.java))
-        finish()
 
+        setContentView(R.layout.activity_splash)
 
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, AddSongActivity::class.java))
+            finish()
+        }, 2000)
     }
 
 }
